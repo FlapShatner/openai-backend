f114ca76ac555f6db0c7260f1eb3bfbb2852169b
Jordan Roberts <jrobertswebdev@gmail.com>
2024-01-23 14:33:34 -0600

    bypass bg removal

--- cloudinary.js
+++ cloudinary.js
@@ -17,7 +17,7 @@ cloudinary.config({
 /////////////////////////
 // Uploads an image file
 /////////////////////////
-export const uploadImage = async (noBgData) => {
+export const uploadImage = async (url) => {
   // Use the uploaded file's name as the asset's public ID and
   // allow overwriting the asset with new versions
   const options = {
@@ -30,16 +30,19 @@ export const uploadImage = async (noBgData) => {
   try {
     // Upload the image
     // const result = cloudinary.uploader.upload_stream(noBgData, options)
-    const result = await new Promise((resolve) => {
-      cloudinary.uploader.upload_stream(options, (error, result) => {
-        if (result) {
-          resolve(result);
-        } else {
-          console.log(error);
-        }
-      }).end(noBgData);
-    })
-    // console.log(result)
+    // const result = await new Promise((resolve) => {
+    //   cloudinary.uploader.upload_stream(options, (error, result) => {
+    //     if (result) {
+    //       resolve(result);
+    //     } else {
+    //       console.log(error);
+    //     }
+    //   }).end(noBgData);
+    // })
+    // // console.log(result)
+    // return result.secure_url
+
+    const result = await cloudinary.uploader.upload(url, options)
     return result.secure_url
   } catch (error) {
     console.error(error)
